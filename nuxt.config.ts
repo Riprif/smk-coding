bded7ecf75df9906c636fb97ae96ee91db9ad3bb
riprif <archkimn@gmail.com>
2023-11-16 21:58:24 +0700

    Supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,31 @@
-const appConfig = {
-  apikey: process.env.SUPABASE_API_KEYS,
-  baseUrl: process.env.SUPABASE_URL,
-  secretKey: process.env.SUPABASE_SECRETS_KEY,
-  storageUrl: process.env.SUPABASE_STORAGE_URL,
-  baseStorageUrl: process.env.SUPABASE_GET_STORAGE_URL,
-};
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"}
+        {
+          rel: "stylesheet", href:
+            "https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
+        }
       ],
-    },
-    // Gunakan appConfig di sini
-    ...appConfig,
+    }
   },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@vite-pwa/nuxt', '@pinia/nuxt'],
+
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "~/tailwind.config.ts",
   },
+
+  appConfig: {
+    apikey: process.env.SUPABASE_API_KEYS,
+    baseUrl: process.env.SUPABASE_URL,
+    secretKey: process.env.SUPABASE_SECRETS_KEY,
+    storageUrl: process.env.SUPABASE_STORAGE_URL,
+    baseStorageUrl: process.env.SUPABASE_GET_STORAGE_URL,
+    },
+    
   pwa: {
     manifest: {
       name: "smkcoding",
